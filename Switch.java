f9eaef619762638069bb0414e9d221c0e00312b0
maquisonsilva <41831227+maquisonsilva@users.noreply.github.com>
2026-02-02 13:26:58 -0300

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,71 @@
+package secao4;
+
+public class Switch {
+    public static void main(String[] args) {
+        
+        // 11 - switch case e break
+
+        // para validar dia de semana baseado no número
+        // 1 = Domingo
+        // 7 = Sábado
+
+        int diaDaSemana = 1;
+
+        switch (diaDaSemana) {
+
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;           
+        }
+
+        // default - obs: o default é o "else" do switch  
+
+        int n = 10;
+
+        switch (n) {
+            case 1:
+                System.out.println("É 1");
+                break;
+            case 2:
+                System.out.println("É 2");
+                break;        
+            default:
+                System.out.println("Número não encontrado");
+                break;
+        }
+
+        // switch sem break
+
+        switch (1) {
+            case 1:
+            System.out.println("Executou 1");
+            case 2:
+            System.out.println("Executou 2");
+            case 3:
+            System.out.println("Executou 3");                        
+            default:
+            System.out.println("Executou 4");
+        }
+
+
+    }
+    
+}
